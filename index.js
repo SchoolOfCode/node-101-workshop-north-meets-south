45492e0c7c36b51f3db49a0c97f8faecdfc06103
troywebber <troywebb95@gmail.com>
2021-12-06 23:18:46 +0000

    finished workshop

---
+++ index.js
@@ -0,0 +1,19 @@
+import myCollection from "./collection.js";
+import chalk from "chalk";
+
+  
+  function describeItem(item) {
+      if (item.count === 1) {
+          console.log(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}`);
+      } else console.log(`I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}s. Here's what I like about them: ${chalk.green(item.whatILike)}`);
+  }
+
+
+  function describeCollection(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let arrayItem = arr[i];
+        describeItem(arrayItem);
+    }
+  }
+
+describeCollection(myCollection);
